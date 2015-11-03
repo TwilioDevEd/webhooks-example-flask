f54284def4a80d825d837ee62e5802f53817ea02
Andrew T. Baker <abaker@twilio.com>
2015-11-03 17:56:17 -0500

    Adding /voice and /message URLs

--- app.py
+++ app.py
@@ -1,9 +1,30 @@
 from flask import Flask
+from twilio import twiml
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
+@app.route('/voice', methods=['POST'])
+def incoming_call():
+    """Twilio Voice URL - receives incoming calls from Twilio"""
+    # Create a new TwiML response
+    resp = twiml.Response()
+
+    # <Say> a message to the caller
+    resp.say("Thanks for calling! I got your call because of Twilio's webhook. Goodbye!")
+
+    # Return the TwiML
+    return str(resp)
+
+@app.route('/message', methods=['POST'])
+def incoming_message():
+    """Twilio Messaging URL - receives incoming messages from Twilio"""
+    # Create a new TwiML response
+    resp = twiml.Response()
+
+    # <Say> a message to the caller
+    resp.message("Thanks for your text! Webhooks are neat :)")
+
+    # Return the TwiML
+    return str(resp)
 
 if __name__ == '__main__':
     app.run()
